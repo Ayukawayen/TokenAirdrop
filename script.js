c798c520f37cec1543824177010688691f29f0e0
Ayukawayen <Ayukawayen@Gmail.com>
2022-02-05 20:38:06 +0800

    feat: update error message

--- script.js
+++ script.js
@@ -130,6 +130,10 @@ async function handleSubmit() {
 				return 'Error: ' + ex.error.data.originalError.message;
 			} else if(ex.error && ex.error.message) {
 				return 'Error: ' + ex.error.message;
+			} else if(ex.data && ex.data.data && ex.data.data.message) {
+				return 'Error: ' + ex.data.data.message;
+			} else if(ex.data && ex.data.message) {
+				return 'Error: ' + ex.data.message;
 			}
 			return 'Something went wrong. Please reload the page and try again.';
 		})(ex);
